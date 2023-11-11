8132377a368fb08722913919c5891bf87d55029e
NazhD <warmagik@yandex.ru>
2023-11-11 12:37:32 +0500

    added dz MOdeuly

--- newPcckage1/employee.py
+++ newPcckage1/employee.py
@@ -2,9 +2,9 @@ class Employee:
 
     def __init__(self, name):
         self.name = name
-        self.salary = 1000
-        self.cost_hour = 10
-        self.comis = 10
+        self.salary = 1000    # оклад
+        self.cost_hour = 10   # стоимость часа
+        self.comis = 10       # комиссия за единицу проданного товара
 
 
 
