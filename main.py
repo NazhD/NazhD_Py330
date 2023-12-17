b4ba83c9305d667b3f57132552c0c9492d55de0f
NazhD <warmagik@yandex.ru>
2023-12-17 12:34:16 +0500

    added dz SQ

--- main.py
+++ main.py
@@ -5558,6 +5558,42 @@ import tkinter
 #     )""")
 
 
+# import sqlite3
+#
+# with sqlite3.connect("education.db") as con:
+#     cur = con.cursor()
+#
+#     cur.execute("""CREATE TABLE IF NOT EXISTS student(
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     surname TEXT NOT NULL,
+#     name TEXT NOT NULL,
+#     patronymic TEXT,
+#     age INTEGER NOT NULL CHECK (age >= 16 AND age <= 45),
+#     group TEXTNOT NULL,
+#     FOREIGN KEY (group) REFERENCES groups (id) ON DELETE RESTRICT
+#     )
+#     """)
+#     cur.execute("""CREATE TABLE IF NOT EXISTS groups(
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     group_name TEXT NOT NULL
+#     )
+#     """)
+#
+#     cur.execute("""CREATE TABLE IF NOT EXISTS association(
+#       lesson_id INTEGER NOT NULL,
+#       group_id INTEGER NOT NULL,
+#       FOREIGN KEY (group_id) REFERENCES  lessons(id)
+#       FOREIGN KEY (lesson_id) REFERENCES groups (id)
+#       )
+#       """)
+#
+#     cur.execute("""CREATE TABLE IF NOT EXISTS lessons(
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     lesson_title TEXT NOT NULL
+#     )
+#     """)
+
+
 
 
 
