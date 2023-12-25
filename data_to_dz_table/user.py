6269dc7ab5d719ff313f96681c5b0f76b9990d63
NazhD <warmagik@yandex.ru>
2023-12-25 12:14:44 +0500

    added dz BASE

--- data_to_dz_table/user.py
+++ data_to_dz_table/user.py
@@ -7,17 +7,17 @@ class Users(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250))
+    name = Column(String(250), nullable=False)
     steam_id = Column(String(250))
-    date_of_registration = Column(String(250))
+    date_of_reg = Column(String(250))
 
-    def __init__(self, name,steam_id,date_of_registration):
+    def __init__(self, name,steam_id,date_of_reg):
         self.name = name
         self.steam_id = steam_id
-        self.date_of_registration = date_of_registration
+        self.date_of_reg = date_of_reg
 
     def __repr__(self):
-        return f"Пользователь (Никнейм: {self.name} Стим id: {self.steam_id} Дата регистрации: {self.date_of_registration})"
+        return f"Пользователь (Никнейм: {self.name} Стим id: {self.steam_id} Дата регистрации: {self.date_of_reg})"
 
 
 
